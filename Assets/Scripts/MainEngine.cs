6613275baff0e4627db14d70aa1f7a19a8b832bb
BJ Rafferty <bjrafferty2000@gmail.com>
2019-04-16 21:04:28 -0400

    Stufff don

---
+++ Assets/Scripts/MainEngine.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainEngine : MonoBehaviour
+{
+    public static MainEngine S;
+    public static int currentRound = 0;
+    public int zombiesPerRound = 5;
+    public int[] maxZPerRound = new int[] { 5, 10, 15, 20 };
+    public int zombiesSpawned = 0;
+    public int runTicks = 0;
+
+    void Start() {
+        S = this;
+    }
+
+    private void Update() {
+        runTicks++;
+    }
+
+
+}
---
+++ Assets/Scripts/ZSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ZSpawner : MonoBehaviour {
+
+    public GameObject zombiePrefab;
+    void Start() {
+        //Invoke("SpawnZombie", 1.5f);
+    }
+
+    private void Update() {
+        int maxZombies = MainEngine.S.maxZPerRound[MainEngine.currentRound];
+
+        if(MainEngine.S.zombiesSpawned < maxZombies) {
+            if(MainEngine.S.runTicks % 150 == 149) {
+                SpawnZombie();
+            }
+
+        }
+    }
+    private void SpawnZombie() {
+        GameObject spawner = GameObject.Find("ZombieSpawner");
+
+        GameObject zombie = Instantiate<GameObject>(zombiePrefab);
+        zombie.transform.position = spawner.transform.position;
+
+        MainEngine.S.zombiesSpawned++;
+    }
+}
